b56761530ee84309a1fdbc27d8e8613f260237bc
Andy Chhuon <chhuonandy@gmail.com>
2021-08-22 19:50:28 -0400

    added function to set owner of smart contract

--- mix/contracts/SimpleCollectible.sol
+++ mix/contracts/SimpleCollectible.sol
@@ -3,10 +3,16 @@ pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
-    constructor () public ERC721 ("Picasso Musks", "MUSK"){
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    address private _owner;
+
+
+    constructor () public ERC721 ("Picasso_Musks", "MUSK"){
         tokenCounter = 0;
+        _setOwner(msg.sender);
     }
 
     function createCollectible(string memory tokenURI) public returns (uint256) {
@@ -15,6 +21,34 @@ contract SimpleCollectible is ERC721 {
         _setTokenURI(newItemId, tokenURI);
         tokenCounter = tokenCounter + 1;
         return newItemId;
+    }
+
+     function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+
+    function _msgData() internal override view virtual returns (bytes memory data) {
+        return msg.data;
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    modifier onlyOwner() {
+        require(owner() == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
     }
 
 }
