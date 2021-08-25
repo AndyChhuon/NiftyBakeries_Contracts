80ee684df391df01df131a755d2c38afc14f5c10
Andy Chhuon <chhuonandy@gmail.com>
2021-08-25 09:03:31 -0400

    fixed naming issue

--- Nft_deploy_main/scripts/simple_collectible/Get_abi.py
+++ Nft_deploy_main/scripts/simple_collectible/Get_abi.py
@@ -1,6 +1,6 @@
 import json
 
-with open("build/Contracts/SimpleCollectible.json") as f:
+with open("build/Contracts/Nft_Collectible_Contract.json") as f:
     info_json = json.load(f)
 abi = info_json["abi"]
 
