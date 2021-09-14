85f92f4aa7f88fd2a5023f64613362cd0e00c4cb
Andy Chhuon <chhuonandy@gmail.com>
2021-09-14 16:36:53 -0400

    Deleted useless stuff

--- Nft_deploy_main/scripts/simple_collectible/Get_abi.py
+++ Nft_deploy_main/scripts/simple_collectible/Get_abi.py
@@ -1,10 +1,13 @@
 #Script get abi for contract
 
+
 import json
+PATH = r'C:\Users\andyc\OneDrive\Desktop\Visual Studio Code\Nft_Deployment_Website\Simple_website_preorder\Nft_Collectible_abi.json'
 
 def main():
     with open("build/Contracts/Nft_Collectible_Contract.json") as f:
         info_json = json.load(f)
     abi = info_json["abi"]
-
+    with open(PATH, 'w') as outfile:
+        json.dump(abi, outfile)
     print(abi)
